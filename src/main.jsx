cc54c8ed5d7d4bc807ef4255d99d8a3c6c2d8030
derrickbanks-hue <derrickbanks@gmail.com>
2026-06-05 19:28:40 -0500

    Conditionally render Parent or App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Parent from './Parent.jsx'
+
+const isParent = window.location.pathname.startsWith('/parent')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isParent ? <Parent /> : <App />}
   </React.StrictMode>
 )
